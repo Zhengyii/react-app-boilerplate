889aa16009ee1ce7e275a3c51f31fdd30d45f41c
ZhengYi <zhengyi@induschain.cn>
2021-09-09 15:38:10 +0800

    feat: 🐧配置ReactRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
+import Login from './pages/User/Login'
 
 function App() {
   return (
-    <Fragment>
-      <div className="App">
-        <h1>This is React App.</h1>
-      </div>
-      <div className="App-other">
-        <h1>This is React App-ohter.</h1>
-      </div>
-    </Fragment>
+    <div className="App">
+      <HashRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Redirect to={'/login'} />
+        </Switch>
+        {/* <div className="App">
+          <h1>This is React App.</h1>
+        </div>
+        <div className="App-other">
+          <h1>This is React App-ohter.</h1>
+        </div> */}
+      </HashRouter>
+    </div>
   )
 }
 
