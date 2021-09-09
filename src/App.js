08cd1463961861aae8e0ae73ae41cdaf2dc54906
ZhengYi <zhengyi@induschain.cn>
2021-09-09 15:43:10 +0800

    init: 🎉 init project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
-import Login from './pages/User/Login'
+import MapDemo from './pages/MapDemo'
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
         <Switch>
-          <Route path="/login" component={Login} />
-          <Redirect to={'/login'} />
+          <Route path="/map" component={MapDemo} />
+          <Redirect to={'/map'} />
         </Switch>
-        {/* <div className="App">
-          <h1>This is React App.</h1>
-        </div>
-        <div className="App-other">
-          <h1>This is React App-ohter.</h1>
-        </div> */}
       </HashRouter>
     </div>
   )
